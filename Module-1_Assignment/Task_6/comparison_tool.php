7d115c6bf35b037b6dff5ecf6045f7beba2405d5
Mahadi Hasan <mhasan201431@bscse.uiu.ac.bd>
2023-09-14 09:54:38 +0600

    "Task_6 Done!"

--- Module-1_Assignment/Task_6/comparison_tool.php
+++ Module-1_Assignment/Task_6/comparison_tool.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Weather reoprt</title>
+    <title>Comparison tool</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
@@ -17,12 +17,12 @@
 
         .head {
             display: block;
-            width: 90%;
+            width: 70%;
             margin: 0 auto;
         }
 
         .box {
-            height: 300px;
+            height: 400px;
             width: 650px;
             box-shadow: 20px 20px 20px grey;
             margin: 40px auto;
@@ -55,11 +55,13 @@
 
         table,
         tr,
-        td {
+        td,
+        th {
+            width: 650px;
             border: 2px solid black;
-            padding: 20px;
-            text-align: center;
             margin: 0 auto;
+            text-align: center;
+            padding: 10px;
         }
     </style>
 
@@ -69,35 +71,32 @@
 
     <div class="container">
         <div class="head">
-            <h1 class="heading bg-primary">Weather Reoprt</h1>
+            <h1 class="heading bg-primary">Comparison Tool</h1>
         </div>
         <div class="box">
             <form action="" method="POST">
 
-                <label for="temp">Enter the Temperature: </label><br>
-                <input type="temp" name="temp" id="" required><br>
+                <label for="num1">Enter 1st Number: </label><br>
+                <input type="text" name="num1" id="" required><br>
 
-                <button class="bg-primary" type="temp">Weather Info</button><br><br>
+                <label for="num2">Enter 2nd Number: </label><br>
+                <input type="text" name="num2" id="" required><br>
+
+                <button class="bg-primary" type="submit">Compare Now</button><br><br>
 
 
                 <?php
 
                 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                    $temperature = $_POST['temp'];
+                    $num1 = $_POST['num1'];
+                    $num2 = $_POST['num2'];
 
 
-                    if ($temperature <= 0) {
-                        $message = "It's Freezing";
-                    } elseif ($temperature > 0 && $temperature <= 20) {
-                        $message = "It's Cool";
-                    } elseif ($temperature > 19 && $temperature <= 35) {
-                        $message = "It's Warm";
-                    } else {
-                        $message = "It's Hot";
-                    }
-                    if (isset($message)) {
+                    $result = ($num1 === $num2) ? "Both are equal number" : (($num1 > $num2) ? "First number: {$num1} is the large number" : "Second number: {$num2} is the large number");
 
-                        echo $message;
+                    if (isset($result)) {
+
+                        echo $result;
                     }
                 }
 
@@ -106,29 +105,6 @@
             </form>
         </div>
     </div>
-
-    <h3 style="text-align: center;">This is the Temperature Range</h3>
-    <table>
-        <tr>
-            <th>Temperature</th>
-            <td>-infinity to 0</td>
-            <td>01 to 20</td>
-            <td>21 to 35</td>
-            <td>35 to +infinity</td>
-
-        </tr>
-
-        <tr>
-            <th>Weathe Info</th>
-            <td>"It's Freezing</td>
-            <td>"It's Cool"</td>
-            <td>It's Warm</td>
-            <td>It's Hot</td>
-
-        </tr>
-
-    </table>
-
 
 </body>
 
