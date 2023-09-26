3ff4765a7bb6b08420de12f688cadc3295234e73
Mahadi Hasan <mhasan201431@bscse.uiu.ac.bd>
2023-09-27 01:39:05 +0600

    task 5 updated.

--- Module-3_Assignment/task_5.php
+++ Module-3_Assignment/task_5.php
@@ -2,19 +2,37 @@
 
 $length = 12;
 
+
 function generatePassword($length)
 {
-    $symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
-    $password = "";
 
-    for ($i = 0; $i < $length; $i++) {
-        $randomIndex = rand(0, (strlen($symbols) - 1));
-        $password .= $symbols[$randomIndex];
+    $lowercase = "abcdefghijklmnopqrstuvwxyz";
+    $uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $numbers = "0123456789";
+    $specialChars = "!@#$%^&*()_+";
+
+
+    $allCharacters = $lowercase . $uppercase . $numbers . $specialChars;
+
+
+    $password = "";
+    $password .= $lowercase[rand(0, strlen($lowercase) - 1)];
+    $password .= $uppercase[rand(0, strlen($uppercase) - 1)];
+    $password .= $numbers[rand(0, strlen($numbers) - 1)];
+    $password .= $specialChars[rand(0, strlen($specialChars) - 1)];
+
+    for ($i = 0; $i < $length - 4; $i++) {
+        $randomIndex = rand(0, strlen($allCharacters) - 1);
+        $password .= $allCharacters[$randomIndex];
     }
+
+
+    $password = str_shuffle($password);
 
     return $password;
 }
 
+
 $password = generatePassword($length);
 
-echo "Generated Password is: $password\n";
+echo "generated Password is: $password\n";
